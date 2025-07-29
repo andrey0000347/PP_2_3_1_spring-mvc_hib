b215317d83e6baa9adb52ab3f52097e6547b3ba9
andrey0000347 <alebedev02@gmail.com>
2025-07-29 20:51:48 +0300

    Update UserServiceImpl.java

--- spring-mvc-no-boot/src/main/java/com/example/service/UserServiceImpl.java
+++ spring-mvc-no-boot/src/main/java/com/example/service/UserServiceImpl.java
@@ -3,7 +3,7 @@ package com.example.service;
 import com.example.model.User;
 import com.example.repository.UserRepository;
 import org.springframework.stereotype.Service;
-
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -22,11 +22,11 @@ public class UserServiceImpl implements UserService {
     public User findUserById(Long id) {
         return userRepository.findById(id);
     }
-
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
-
+    @Transactional
     public void deleteUserById(Long id) {
         userRepository.deleteById(id);
     }
